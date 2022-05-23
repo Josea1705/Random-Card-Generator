c4e9fb2f52a1fc492aaae62ec873bdaed3d57ebb
Josea1705 <josebarreto665@gmail.com>
2022-05-23 21:45:47 +0000

    cambios para numeros aleatorios y simbolos

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let generaterandomnumber = () => {
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "K",
+      "Q"
+    ];
+    let indexnumbers = Math.floor(Math.random() * numbers.length);
+    return numbers[indexnumbers];
+  };
+
+  let generaterandompica = () => {
+    let symbols = ["♦", "♥", "♠", "♣"];
+    let indexsymbols = Math.floor(Math.random() * symbols.length);
+    return symbols[indexsymbols];
+  };
+
+  let newsymbol = generaterandompica();
+  document.querySelector(".top-card").textContent = newsymbol;
+  document.querySelector(".bottom-card").textContent = newsymbol;
+  document.querySelector(".number").innerHTML = generaterandomnumber();
 };
